b0118e16ff2472f875b90e42138d888ffcf1cc9f
Jakub Kalina <jakub.kalina@codeteam.pl>
2020-01-06 00:25:09 +0100

    Started implementing the rest of user management posibilities

--- BusLines/Controllers/AccountController.cs
+++ BusLines/Controllers/AccountController.cs
@@ -135,5 +135,207 @@ namespace BusLines.Controllers
         }
 
 
+        /// <summary>
+        ///     Bierze obecnie zalogowanego użytkownika i wypełnia jego danymi formularz
+        ///     do edycji danych, który następnie zwraca do użytkownika.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> EditProfile()
+        {
+            // Wyszukanie loginu aktualnie zalogowanego użytkownika
+            var user = HttpContext.User.Identity.Name;
+
+            //var user = await _accountService.FindUserByUserNameAsync(HttpContext.User.Identity.Name);
+            //var model = _mapper.Map<ApplicationUser, AccountEditProfileApplicationUserViewModel>(user);
+            //return View(model);
+            return View();
+        }
+
+        /// <summary>
+        ///     Wysyła polecenie edycji profilu użytkownika i nadpisanie edytowanych pól użytkownika (np. imię).
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> EditProfile(AccountEditProfileApplicationUserViewModel model)
+        //{
+        //    //if (!ModelState.IsValid) return View(model);
+        //    //var user = await _accountService.FindUserByUserNameAsync(HttpContext.User.Identity.Name);
+
+        //    //_mapper.Map(model, user);
+
+        //    //var result = await _accountService.UpdateUserAsync(user);
+
+        //    //if (result.Succeeded)
+        //    //    ViewBag.Message = "Profil został zaktualizowany";
+        //    //else
+        //    //    ModelState.AddModelError("", "Wystąpił błąd podczas zapisu");
+
+        //    //return View(model);
+        //}
+
+        /// <summary>
+        ///     Zwraca formularz do zmiany hasła.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Authorize]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        /// <summary>
+        ///     Wysyła polecenie serwisowi aby ten zmienił hasło dla obecnie zalogowanego użytkownika.
+        ///     Użytkownik po zmianie hasła jest wylogowywany i proszony o ponowne zalogowanie z użyciem nowego hasła.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> ChangePassword(AccountChangePasswordApplicationUserViewModel model)
+        //{
+        //    //if (!ModelState.IsValid) return View(model);
+        //    //var user = await _accountService.FindUserByUserNameAsync(HttpContext.User.Identity.Name);
+        //    //var result = await _accountService.ChangeUserPasswordAsync(user.Id, model.OldPassword, model.NewPassword);
+        //    //if (result.Succeeded) return RedirectToAction("Login", "Account");
+
+        //    //AddErrors(result);
+
+        //    //return View(model);
+        //}
+
+        /// <summary>
+        ///     Widok wyświetlany po kliknięciu w link aktywacyjny użytkownika, który zarejestrował się przez API.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult ConfirmEmailApi()
+        {
+            return View();
+        }
+
+        /// <summary>
+        ///     Widok wyświetlany po kliknięciu w link aktywacyjny użytkownika,
+        ///     który zarejestrował się przez MVC,
+        ///     pod warunkiem, że potwierdzenie adresu email się powiodło.
+        /// </summary>
+        /// <param name="userId">id użytkownika</param>
+        /// <param name="code">token do weryfikacji</param>
+        /// <returns></returns>
+        //[HttpGet]
+        //[AllowAnonymous]
+        //public async Task<IActionResult> ConfirmEmail(string userId, string code)
+        //{
+        //    //if (userId == null || code == null) return View("Error");
+        //    //var result = await _accountService.ConfirmUserEmailAsync(userId, code);
+        //    //return View(result.Succeeded ? "ConfirmEmail" : "Error");
+        //}
+
+        /// <summary>
+        ///     Widok dla przypomnienia hasła, w którym użytkownik wprowadza adres email,
+        ///     na który ma zostać wysłany link do resetowania hasła.
+        /// </summary>
+        /// <returns></returns>
+        //[HttpGet]
+        //[AllowAnonymous]
+        //public IActionResult ForgotPassword()
+        //{
+        //    return View();
+        //}
+
+        /// <summary>
+        ///     Wysyła polecenie, by sprawdzić czy użytkownik potwierdził email.
+        ///     Jeśli potwierdził, to wysyła na adres email link do zresetowania hasła.
+        ///     Jeśli nie potwierdził, to wysyłany jest nowy link aktywacyjny na email.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        //[HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> ForgotPassword(AccountForgotPasswordApplicationUserViewModel model)
+        //{
+        //    //if (!ModelState.IsValid) return View(model);
+        //    //var user = await _accountService.FindUserByEmailAsync(model.Email);
+
+        //    //if (user == null)
+        //    //    // Nie pokazuj, że użytkownika nie ma w bazie
+        //    //    return View("ForgotPasswordConfirmation");
+
+        //    //var isUserEmailConfirmed = await _accountService.IsUserEmailConfirmedAsync(user.Id);
+
+        //    //// W zależności od stanu potwierdzenia adresu użytkownika wyśle link aktywujący konto lub link do resetu hasła
+        //    //await _accountService.ForgotPasswordAsync(user.Id);
+
+        //    //return isUserEmailConfirmed
+        //    //    ? (ActionResult)RedirectToAction("ForgotPasswordConfirmation", "Account")
+        //    //    : View("PleaseConfirmEmail");
+        //}
+
+        /// <summary>
+        ///     Widok potwierdzający zmianę hasła - informuje
+        ///     użytkownika, by udał się na swoją skrzynkę mailową
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult ForgotPasswordConfirmation()
+        {
+            return View();
+        }
+
+        /// <summary>
+        ///     Zwraca widok do resetowania hasła.
+        /// </summary>
+        /// <param name="code">token</param>
+        /// <returns></returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult ResetPassword(string code)
+        {
+            return code == null ? View("Error") : View();
+        }
+
+        /// <summary>
+        ///     Resetuje hasło użytkownika i ustawia nowe zadane przez użytkownika.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        //[HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> ResetPassword(AccountResetPasswordApplicationUserViewModel model)
+        //{
+        //    //if (!ModelState.IsValid) return View(model);
+        //    //var user = await _accountService.FindUserByUserNameAsync(model.UserName);
+        //    //if (user == null)
+        //    //    // Nie pokazuj, że użytkownika nie ma w bazie
+        //    //    return RedirectToAction("ResetPasswordConfirmation", "Account");
+        //    //var result = await _accountService.ResetUserPasswordAsync(user.Id, model.Code, model.Password);
+        //    //if (result.Succeeded) return RedirectToAction("ResetPasswordConfirmation", "Account");
+        //    //AddErrors(result);
+        //    //return View();
+        //}
+
+        /// <summary>
+        ///     Widok potwierdzający zresetowanie hasła.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult ResetPasswordConfirmation()
+        {
+            return View();
+        }
+
     }
 }
+
+
+// Gdy użytkownik zapomni hasła, to jego hasło jest zmieniane na hash i wysyłane na podany adres email
+// Loguje się za pomocą hashu i wtedy ma możliwość zmiany hasła dopiero w aplikacji po zalogowaniu
---
+++ Logic/ViewModels/Account/EditProfileViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Logic.ViewModels.Account
+{
+    public class EditProfileViewModel
+    {
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "Imię")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "Nazwisko")]
+        public string Surname { get; set; }
+
+        [Required]
+        [StringLength(11)]
+        [Display(Name = "Numer telefonu")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "Adres email")]
+        public string EmailAddress { get; set; }
+    }
+}
